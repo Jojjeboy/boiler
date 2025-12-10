bbe56f3be23d13b839fa7c09e32b972f5fb32ef1
Johannes Kärnstam <johka96@vgregion.se>
2025-12-10 16:43:55 +0100

    What a Vue

--- src/app/postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: {
-    // Lägg till Tailwind CSS i PostCSS-pipeline
-    'tailwindcss': {},
-    // Lägg till Autoprefixer för att hantera webbläsarprefix
-    'autoprefixer': {},
-  }
-}
--- upload-queue.service.ts
+++
@@ -1,1 +0,0 @@
-// upload queue service placeholder
